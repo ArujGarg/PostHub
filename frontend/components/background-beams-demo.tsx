f918932c6a9863b28f0beeaee2f947500962a465
Aruj Garg <arujgarg04@gmail.com>
2025-02-24 23:08:13 +0530

    landing page started

---
+++ frontend/components/background-beams-demo.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { LitButton } from "./LitButton"
+import { BackgroundBeams } from "./ui/background-beams"
+
+
+export default function BackgroundBeamsDemo() {
+  return (
+    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center antialiased ">
+      <div className="max-w-2xl mx-auto p-4 mt-16">
+        <h1 className="relative z-10 text-lg md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300 text-center font-sans font-bold">
+            PostHub
+        </h1>
+        <p></p>
+        <p className="text-neutral-300 max-w-lg mx-auto my-2 text-xl text-center relative z-10">
+        A text-based social media platform to express your thoughts and connect through words.
+        </p>
+      </div>
+      <div>
+      <BackgroundBeams />
+      </div>
+      <div>
+        <LitButton text="Get started" />
+      </div>
+    </div>
+  )
+}
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import BackgroundBeamsDemo from '../components/background-beams-demo'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <BackgroundBeamsDemo />
+    </>
+  )
+}
+
+export default App
