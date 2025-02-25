9eab560bd8c2a7a25afec841362ce551837c26bc
Aruj Garg <arujgarg04@gmail.com>
2025-02-26 01:44:14 +0530

    minor fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import BackgroundBeamsDemo from '../components/background-beams-demo'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
-import { Navbar } from '../components/Navbar'
 import { SignupFormDemo } from './pages/Signup'
+import { SigninFormDemo } from './pages/Signin'
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/landing' element={<BackgroundBeamsDemo />} />
           <Route path='/signup' element={<SignupFormDemo />} />
+          <Route path='/signin' element={<SigninFormDemo />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -11,6 +11,7 @@ import { cn } from "../../utils/cn";
 import { Input } from "../../components/ui/input";
 import { Label } from "../../components/ui/label";
 import { BackgroundBeams } from "../../components/ui/background-beams";
+import { Link } from "react-router-dom";
 
 export function SignupFormDemo() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -22,13 +23,14 @@ export function SignupFormDemo() {
       <div className="fixed inset-0 -z-10 bg-neutral-900 h-screen ">
         <BackgroundBeams />
       </div>
-      <div className="max-w-md w-full mx-auto mt-16 mb-12 rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-neutral-950 ">
+      <div className="max-w-md w-full mx-auto my-16 rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-neutral-950 ">
         <div className="flex flex-col items-center">
           <h2 className="font-bold text-2xl text-neutral-800 dark:text-neutral-200">
             Signup to Posthub
           </h2>
           <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-            Already have an account? Signin
+            Already have an account? 
+            <Link className="underline pl-2 hover:text-violet-500" to={"/signin"}>Login</Link>
           </p>
         </div>
 
@@ -53,7 +55,7 @@ export function SignupFormDemo() {
           </LabelInputContainer>
 
           <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] cursor-pointer"
             type="submit"
           >
             Sign up &rarr;
@@ -64,7 +66,7 @@ export function SignupFormDemo() {
 
           <div className="flex flex-col space-y-4">
             <button
-              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] cursor-pointer"
               type="submit"
             >
               <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
@@ -74,7 +76,7 @@ export function SignupFormDemo() {
               <BottomGradient />
             </button>
             <button
-              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] cursor-pointer"
               type="submit"
             >
               <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
