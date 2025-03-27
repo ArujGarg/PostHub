ce796d8c78fc355d518c6af0ce2690bf125cd13a
Aruj Garg <arujgarg04@gmail.com>
2025-03-27 18:27:48 +0530

    small change

--- frontend/src/pages/Post.tsx
+++ frontend/src/pages/Post.tsx
@@ -50,28 +50,3 @@ export function Post(){
     </div>
 }
 
-
-
-function Skeleton(){
-    return (
-        <div>
-            <div className="w-[97.7%] rounded-md border border-neutral-800 p-4 m-2 ">
-                <div className="flex animate-pulse space-x-4 pr-6">
-                    <div className="size-12 rounded-full bg-gray-200"></div>
-                    <div className="flex-1 space-y-4 py-1">
-                        <div className="h-2 rounded bg-gray-200 w-[80px]"></div>
-                        <div className="h-2 rounded bg-gray-200 w-[80px]"></div>
-                        <div className="space-y-3">
-                            <div className="grid grid-cols-3 gap-4">
-                                <div className="col-span-2 h-2 rounded bg-gray-200"></div>
-                                <div className="col-span-1 h-2 rounded bg-gray-200"></div>
-                            </div>
-                            <div className="h-2 rounded bg-gray-200"></div>
-                        </div>
-                        <div className="h-2 rounded bg-gray-200 w-[80px]"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
